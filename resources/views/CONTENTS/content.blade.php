fc0271efdff107f870f4a624150bf4352e939733
Fuujin <133795177+jhoncarlnayao@users.noreply.github.com>
2025-01-08 23:56:13 +0800

    NOTHING ADDED

--- resources/views/CONTENTS/content.blade.php
+++ resources/views/CONTENTS/content.blade.php
@@ -197,7 +197,7 @@
                         <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Time In</th>
                         <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Time Out</th>
                         <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Status</th>
-                        <th scope="col" class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Action</th>
+                        <th scope="col" class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase dark:text-neutral-500"></th>
                       </tr>
                     </thead>
                   </table>
